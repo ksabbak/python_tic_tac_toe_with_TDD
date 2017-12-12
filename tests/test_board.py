bc171a533abf5d8abd6fd080e54eb337fb0896d3
K. Sabbak <ksabbak@gmail.com>
2017-12-12 16:38:43 -0600

    Refactor tests to use pytest fixtures

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,22 +1,26 @@
+import pytest
+
 from ..app.__init__ import Board
 
-    
-def test_board_has_9_spaces():
-    assert len(Board().spaces) == 9
-
-def test_board_can_mark_space():
+@pytest.fixture()
+def board():
     board = Board()
+    return board
+
+    
+def test_board_has_9_spaces(board):
+    assert len(board.spaces) == 9
+
+def test_board_can_mark_space(board):
     board.mark_space(1, "x")
     assert board.spaces[1] == "x"
 
-def test_board_does_not_remark_space():
-    board = Board()
+def test_board_does_not_remark_space(board):
     board.mark_space(1, "x")
     board.mark_space(1, "o")
     assert board.spaces[1] == "x"
 
-def test_board_knows_when_full():
-    board = Board()
+def test_board_knows_when_full(board):
     assert not board.is_full()
     i = 0
     while i < len(board.spaces):
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,15 +1,20 @@
+import pytest
+
 from ..app.__init__ import Game
 
+@pytest.fixture()
+def game():
+    return Game()
+
 # GENERAL
-def test_there_is_a_game():
-    assert Game() is not None
+def test_there_is_a_game(game):
+    assert game is not None
 
 # BOARD
-def test_game_has_board():
-    assert Game().board is not None
+def test_game_has_board(game):
+    assert game.board is not None
 
-def test_game_is_marked_over_when_board_is_filled():
-    game = Game()
+def test_game_is_marked_over_when_board_is_filled(game):
     i = 0
     while i < len(game.board.spaces):
         game.board.mark_space(i, "X")
@@ -17,11 +22,11 @@ def test_game_is_marked_over_when_board_is_filled():
     assert game.is_over
 
 #PLAYERS
-def test_game_has_two_players():
-    assert len(Game().players) == 2
+def test_game_has_two_players(game):
+    assert len(game.players) == 2
 
-def test_player_one_has_marker_x():
-    assert Game().players[0].marker == "x"
+def test_player_one_has_marker_x(game):
+    assert game.players[0].marker == "x"
 
-def test_player_two_has_marker_o():
-    assert Game().players[1].marker == "o"
+def test_player_two_has_marker_o(game):
+    assert game.players[1].marker == "o"

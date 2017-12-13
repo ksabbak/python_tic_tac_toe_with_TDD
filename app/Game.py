654fb335d6648cb6152f31b0c5c217f48cd698c1
K. Sabbak <ksabbak@gmail.com>
2017-12-13 09:44:17 -0600

    Pass second row win condition test.

--- app/Game.py
+++ app/Game.py
@@ -10,5 +10,6 @@ class Game:
         self.players = [Player(player1), Player(player2)]
 
     def is_over(self):
-        winner = (self.board.spaces[0] == self.board.spaces[1] and self.board.spaces[0] == self.board.spaces[2])
+        winner = (self.board.spaces[0] == self.board.spaces[1] and self.board.spaces[0] == self.board.spaces[2]) 
+        winner = winner or (self.board.spaces[3] == self.board.spaces[4] and self.board.spaces[3] == self.board.spaces[5]) 
         return self.board.is_full() or winner

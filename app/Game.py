feac34aee21af5ab873aea66db68b63699bc32a9
K. Sabbak <ksabbak@gmail.com>
2017-12-13 11:07:35 -0600

    Write and pass test for first vertical win condition

--- app/Game.py
+++ app/Game.py
@@ -10,14 +10,19 @@ class Game:
         self.players = [Player(player1), Player(player2)]
 
     def is_over(self):
-        winner = self._horizontal_win_conditions()
+        winner = (self._horizontal_win_conditions() 
+                  or self._vertical_win_conditions())
         return self.board.is_full() or winner
 
     def _horizontal_win_conditions(self):
         i = 0
         while i < len(self.board.spaces):
-            winner = (self.board.spaces[i] == self.board.spaces[i + 1] and self.board.spaces[i] == self.board.spaces[i+2])
+            winner = (self.board.spaces[i] == self.board.spaces[i + 1] 
+                       and self.board.spaces[i] == self.board.spaces[i+2])
             if winner: return True
             i += 3
         return False
 
+    def _vertical_win_conditions(self):
+        return (self.board.spaces[0] == self.board.spaces[3] 
+                and self.board.spaces[0] == self.board.spaces[6])

2c9ed5eb8d248be7f9e742e6d6d82c3ca86bace2
K. Sabbak <ksabbak@gmail.com>
2017-12-13 16:53:51 -0600

    Clean up commented code from game file

--- app/Game.py
+++ app/Game.py
@@ -18,44 +18,17 @@ class Game:
           or self._vertical_win_conditions()
           or self._diagonal_win_conditions())
         return winner
+        
 
     def _horizontal_win_conditions(self):
         return self._calculate_win_condition(1, self.board.side_length)
-        # i = 0
-        # while i < len(self.board.spaces):
-        #     winner = (self.board.spaces[i] == self.board.spaces[i + 1] 
-        #                and self.board.spaces[i] == self.board.spaces[i+2])
-        #     if winner: return True
-        #     i += 3
-        # return False
 
     def _vertical_win_conditions(self):
         return self._calculate_win_condition(self.board.side_length, 1)
-        # i = 0
-        # while i < ( len(self.board.spaces) / 3):
-        #     winner = (self.board.spaces[i] == self.board.spaces[i + 3] 
-        #                and self.board.spaces[i] == self.board.spaces[i+6])
-        #     if winner: return True
-        #     i += 1
-        # return False
+
 
     def _diagonal_win_conditions(self):
         return self._calculate_win_condition(self.board.side_length+1 , -2)
-        # return ((self.board.spaces[0] == self.board.spaces[4] 
-        #             and self.board.spaces[0] == self.board.spaces[8])
-        #         or
-        #         (self.board.spaces[2] == self.board.spaces[4] 
-        #                and self.board.spaces[2] == self.board.spaces[6]))
-        ####
-        # i = 0
-        # side_length = int((len(self.board.spaces)**(1/2)))
-        # while i <= 2:
-        #     winner = (self.board.spaces[i] == self.board.spaces[side_length + 1] 
-        #                and self.board.spaces[i] == self.board.spaces[2 * (side_length + 1 - i)])
-        #     if winner: return True
-        #     i += 2
-        # return False
-
 
     def _calculate_win_condition(self, addition, incrementor):
         i = 0

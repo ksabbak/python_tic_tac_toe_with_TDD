8a731734f1b431ac24a4f4ba002c4ca7e29dfaf4
K. Sabbak <ksabbak@gmail.com>
2017-12-14 11:43:23 -0600

    Pass get player move test

--- app/player.py
+++ app/player.py
@@ -2,6 +2,8 @@ class Player:
     def __init__(self, marker):
         self.marker = marker
 
-
-
-    
+    def get_move(self):
+        #This is not single responsibility.
+        print("Where would you like to move?")
+        move = input()
+        return move
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,11 @@
+import pytest
+import unittest
 from unittest.mock import patch
+# from unittest import TestCase
 
 from ..app.__init__ import Player
+
+# class TestPlayer(TestCase):
 
 @pytest.fixture()
 def playerx():
@@ -9,6 +14,7 @@ def playerx():
 def test_players_have_markers():
     assert Player("?").marker is not None
 
-@patch('player.get_input', return_value=7)
+# @patch('builtins.input', return_value="7")
 def test_get_move(playerx):
-    assert playerx.get_move() == 7
+    with unittest.mock.patch('builtins.input', return_value='7'):
+        assert playerx.get_move() == "7"

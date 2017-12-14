73516bb35ddde90fe59bff9010e186bcd89c16eb
K. Sabbak <ksabbak@gmail.com>
2017-12-14 16:37:57 -0600

    Pass stop immediate loss test for ai

--- app/ai.py
+++ app/ai.py
@@ -8,7 +8,7 @@ from .win_conditions import winner
 class AI(Player):
     
     def get_move(self, board):
-        move = self._get_winning_move(board) or 0
+        move = self._get_winning_move(board) or self._stop_immidate_loss(board) or 0
         return move
 
     def _get_winning_move(self, board):
@@ -17,8 +17,12 @@ class AI(Player):
             copy_board.mark_space(space, self.marker)
             if winner(copy_board) == self.marker: return space
 
-    def _stop_immidate_loss(self):
-        pass
+    def _stop_immidate_loss(self, board):
+        opponent_marker = self._deduce_opponent_marker(board)
+        for space in range(0, len(board.spaces)):
+            copy_board = copy(board)
+            copy_board.mark_space(space, opponent_marker)
+            if winner(copy_board) == opponent_marker: return space
 
     def _deduce_opponent_marker(self, board):
         for space in range(0, len(board.spaces)):

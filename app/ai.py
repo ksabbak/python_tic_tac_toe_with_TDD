de57d25b69045ada4de2e6469b1a056aa789f2ab
K. Sabbak <ksabbak@gmail.com>
2017-12-14 16:21:56 -0600

    Pass ai winning move test

--- app/ai.py
+++ app/ai.py
@@ -1,4 +1,5 @@
 from random import randint
+from copy import copy
 
 from .player import Player
 from .board import Board
@@ -7,9 +8,14 @@ from .win_conditions import winner
 class AI(Player):
     
     def get_move(self, board):
-        return 0
+        move = self.get_winning_move(board) or 0
+        return move
 
     def get_winning_move(self, board):
-        pass
+        for space in range(0, len(board.spaces)):
+            copy_board = copy(board)
+            copy_board.mark_space(space, self.marker)
+            if winner(copy_board) == self.marker: return space
+
 
 
--- tests/test_board.py
+++ tests/test_board.py
@@ -35,3 +35,5 @@ def test_board_knows_when_space_is_empty(board):
 
 def test_board_to_str(board):
      assert board.to_str() == " 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \n" 
+
+

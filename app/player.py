fcd8b59b29d8045f4eb1b876013cbd384c639bce
K. Sabbak <ksabbak@gmail.com>
2017-12-14 11:19:17 -0600

    Write test for getting user input

--- app/player.py
+++ app/player.py
@@ -1,3 +1,7 @@
 class Player:
     def __init__(self, marker):
         self.marker = marker
+
+
+
+    
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,4 +1,14 @@
+from unittest.mock import patch
+
 from ..app.__init__ import Player
+
+@pytest.fixture()
+def playerx():
+    return Player("x")
 
 def test_players_have_markers():
     assert Player("?").marker is not None
+
+@patch('player.get_input', return_value=7)
+def test_get_move(playerx):
+    assert playerx.get_move() == 7

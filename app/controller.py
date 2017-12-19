cd7eca959690b7522915de4a80b997eb69d59402
K. Sabbak <ksabbak@gmail.com>
2017-12-19 15:06:17 -0600

    Write test for getting human vs human

--- app/controller.py
+++ app/controller.py
@@ -1,3 +1,11 @@
 class Controller:
     def run(self):
-        pass
+        print("Welcome!")
+        print("this is tic-tac-toe!")
+        print("Here are your options: ")
+        print("  1. Human vs. Human")
+        print("  2. Human vs. Computer")
+        print("  3. Computer vs. Computer")
+        print("Please enter the number of your selection: ")
+
+            
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,7 +1,13 @@
 import pytest
+import unittest
+from unittest.mock import patch
 
 from ..app.controller import Controller
 
 def tests_controller_can_be_called():
     controller = Controller()
     assert controller.run
+
+def test_controller_choses_proper_game():
+    with unittest.mock.patch('builtins.input', return_value='1'):
+        assert Controller().run() in "Human vs. Human!"

168eb47853c449b24d57c9e366e2620acd4a5148
K. Sabbak <ksabbak@gmail.com>
2017-12-19 15:09:20 -0600

    Write test for getting Human vs. Comp

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -11,3 +11,5 @@ def tests_controller_can_be_called():
 def test_controller_choses_proper_game():
     with unittest.mock.patch('builtins.input', return_value='1'):
         assert Controller().run() in "Human vs. Human!"
+    with unittest.mock.patch('builtins.input', return_value='2'):
+        assert Controller().run() in "Human vs. Computer!"

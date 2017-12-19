299f2f71b3abd2e530b2efe5e8cf86a2da5000e6
K. Sabbak <ksabbak@gmail.com>
2017-12-19 13:52:11 -0600

    Remove print statements, extraneous methods. Reorganize

--- app/ai.py
+++ app/ai.py
@@ -14,6 +14,9 @@ class AI(Player):
         self._get_move_weights(board)
         return self._pick_the_best_move()
 
+    # PRIVATE METHODS
+
+    # MINIMAX
     def _pick_the_best_move(self):
         best_spaces = []
         most_weight = 0
@@ -25,42 +28,9 @@ class AI(Player):
                 best_spaces = [space]
         return choice(best_spaces)
 
-    def _get_winning_move(self, board):
-        return self._make_immediate_vital_move(board, self.marker)
-
-    def _stop_immidate_loss(self, board):
-        return self._make_immediate_vital_move(board, self.opponent_marker)
-
-    def _make_immediate_vital_move(self, board, marker):
-        for space in board.empty_spaces():
-            copy_board = copy(board)
-            copy_board.mark_space(space, marker)
-            if winner(copy_board) == marker: return space
-
-    def _make_good_choice(self, board):
-        if ((len(board.empty_spaces()) == len(board.spaces) - 1) 
-            and board.space_is_empty(4)):
-            return 4
-        else:
-            return 0
-
-    def _make_best_choice(self, board):
-        move = self._get_winning_move(board) 
-        move = move or self._stop_immidate_loss(board) 
-        move = move or self._make_good_choice(board) 
-        return move
-
-    def _deduce_opponent_marker(self, board):
-        for space in range(0, len(board.spaces)):
-            if (not board.space_is_empty(space) 
-                and (board.spaces[space] != self.marker)):
-                return board.spaces[space]
-
     def _get_move_weights(self, board):
-        print(board.to_str())
         for space in board.empty_spaces():
             self.move_weights[space] = self._best_move(board, space)
-        print(self.move_weights)
 
     def _best_move(self, board, set_move=None, turn="self"):
         if board.is_full(): return 0
@@ -72,35 +42,36 @@ class AI(Player):
             if winner(board2): return 100000000000
         for move in board2.empty_spaces():
             boardcopy = self._copy_the_board(board2)
-            if self._current_spaces(boardcopy, self.marker) <= self._current_spaces(boardcopy, self.opponent_marker) and turn == "self":
-                if self._make_immediate_vital_move(boardcopy, self.marker) is not None:
-                    weight += 10
-                else:
-                    weight += self._calculate_move(boardcopy, move, self.marker, 10)
+            if turn == "self":
+                weight += self._calculate_move(boardcopy, move, self.marker, 10)
             else:
-                if self._make_immediate_vital_move(boardcopy, self.opponent_marker) is not None:
-                    weight -= 10
-                else:
-                    weight += self._calculate_move(boardcopy, move, self.opponent_marker, -10)
+                weight += self._calculate_move(boardcopy, move, self.opponent_marker, -10)
         return weight
 
-
     def _calculate_move(self, board, move, marker, win_weight):
-        board.mark_space(move, marker)
-        if winner(board):
+        if self._make_immediate_vital_move(board, marker) is not None:
             weight = win_weight
         else:
+            board.mark_space(move, marker)
             next_turn = "self" if win_weight < 0 else "other"
             weight = self._best_move(board, turn=next_turn)
         return weight
 
+    def _make_immediate_vital_move(self, board, marker):
+        for space in board.empty_spaces():
+            copy_board = copy(board)
+            copy_board.mark_space(space, marker)
+            if winner(copy_board) == marker: return space
 
+    # MECHANICS
     def _copy_the_board(self, board):
         new_board = Board()
         for space in new_board.spaces:
             new_board.mark_space(space, board.spaces[space])
         return new_board
 
-    def _current_spaces(self, board, marker):
-        return len([space for space in range(0, len(board.spaces)) 
-                if board.spaces[space] == marker])
+    def _deduce_opponent_marker(self, board):
+        for space in range(0, len(board.spaces)):
+            if (not board.space_is_empty(space) 
+                and (board.spaces[space] != self.marker)):
+                return board.spaces[space]
--- tests/test_ai.py
+++ tests/test_ai.py
@@ -13,35 +13,29 @@ def tests_ai_is_a_player():
     assert issubclass(AI, Player)
 
 def tests_ai_can_move_without_input(ai, board):
-    print("------ANY MOVE--------")
     assert ai.get_move(board) in range(0, 9)
 
 def tests_ai_moves_to_empty_spot(ai, board):
     for i in range(1, 9):
         board.mark_space(i, "x")
-    print("------EMPTY SPOT--------")
     assert ai.get_move(board) == 0
 
 def test_ai_can_make_winning_move(ai, board):
     board.mark_space(0, "x")
     board.mark_space(1, "x")
-    print("------WINNING MOVE--------")
     assert ai.get_move(board) == 2
 
 def test_ai_can_stop_immediate_loss(ai, board):
     board.mark_space(3, "o")
     board.mark_space(8, "x")
     board.mark_space(4, "o")
-    print("------LOSING MOVE--------")
     assert ai.get_move(board) == 5
 
 def test_ai_1st_move_when_1st_is_center(ai, board):
-    print("------1st MOVE--------")
     assert ai.get_move(board) == 4
 
 def test_ai_1st_move_when_2nd_is_middle_space_if_open(ai, board):
     board.mark_space(3, "o")
-    print("------2nd MOVE--------")
     assert ai.get_move(board) == 4
 
 def test_ai_prevents_opponent_from_fork_win(ai, board):
@@ -59,15 +53,3 @@ def test_ai_prevents_opponent_from_fork_win_middle(ai, board):
 def test_ai_can_determine_opponent_marker(ai, board):
     board.mark_space(0, "o")
     assert ai._deduce_opponent_marker(board) == "o"
-
-def test_ai_knows_spaces(ai, board):
-    board.mark_space(0, "o")
-    board.mark_space(1, "o")
-    board.mark_space(2, "o")
-    board.mark_space(3, "o")
-    board.mark_space(4, "x")
-    board.mark_space(5, "x")
-    board.mark_space(6, "x")
-    board.mark_space(7, "o")
-    assert ai._current_spaces(board, "o") == 5
-    assert ai._current_spaces(board, "x") == 3

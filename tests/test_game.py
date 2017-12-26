eba1fffb90b8ed0132a726fed7de4aab30d11229
K. Sabbak <ksabbak@gmail.com>
2017-12-26 16:23:01 -0600

    Write test for game.undo

--- tests/test_game.py
+++ tests/test_game.py
@@ -19,6 +19,14 @@ def test_game_is_marked_over_when_board_is_filled(game):
         i += 1
     assert game.is_over() is True
 
+def test_undo_removes_last_move_from_both_players(game):
+    game.players[0].make_move(game.board, 4)
+    game.players[1].make_move(game.board, 5)
+    game.players[0].make_move(game.board, 0)
+    game.undo()
+    assert len(game.players[0].moves) == 1
+    assert len(game.players[1].moves) == 0
+
 # BOARD
 def test_game_has_board(game):
     assert game.board is not None

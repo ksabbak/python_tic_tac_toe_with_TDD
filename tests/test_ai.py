e219785bba0286a885b1d652b56ba898ca1f20cc
K. Sabbak <ksabbak@gmail.com>
2017-12-27 12:05:37 -0600

    write tests for 16 space board, tests are taking forever.

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -23,6 +23,10 @@ def test_ai_player_logs_moves(ai, board):
     move_2 = ai.make_move(board, None)
     assert ai.moves == [move, move_2]
 
+def test_ai_can_determine_opponent_marker(ai, board):
+    board.mark_space(0, "o")
+    assert ai._deduce_opponent_marker(board) == "o"
+
 def tests_ai_can_move_without_input(ai, board):
     assert ai._get_move(board) in range(0, 9)
 
@@ -96,6 +100,78 @@ def test_ai_prevents_opponent_from_fork_win_middle_two_corners(ai, board):
     board.mark_space(0, "o")
     assert ai._get_move(board) in [2, 6]
 
-def test_ai_can_determine_opponent_marker(ai, board):
-    board.mark_space(0, "o")
-    assert ai._deduce_opponent_marker(board) == "o"
+def test_ai_can_make_winning_move(ai, board):
+    board.mark_space(0, "x")
+    board.mark_space(1, "x")
+    assert ai._get_move(board) == 2
+
+def test_ai_can_stop_immediate_horizontal_loss0(ai, board):
+    board.mark_space(3, "o")
+    board.mark_space(0, "x")
+    board.mark_space(4, "o")
+    assert ai._get_move(board) == 5
+
+def test_ai_can_stop_immediate_horizontal_loss2(ai, board):
+    board.mark_space(3, "o")
+    board.mark_space(2, "x")
+    board.mark_space(4, "o")
+    assert ai._get_move(board) == 5
+
+def test_ai_can_stop_immediate_horizontal_loss6(ai, board):
+    board.mark_space(3, "o")
+    board.mark_space(6, "x")
+    board.mark_space(4, "o")
+    assert ai._get_move(board) == 5
+
+def test_ai_can_stop_immediate_horizontal_loss8(ai, board):
+    board.mark_space(3, "o")
+    board.mark_space(8, "x")
+    board.mark_space(4, "o")
+    assert ai._get_move(board) == 5
+
+@pytest.fixture()
+def big_board():
+    return Board(16)
+
+def test_ai_can_make_winning_move(ai, big_board):
+    big_board.mark_space(0, "x")
+    big_board.mark_space(1, "x")
+    big_board.mark_space(3, "x")
+    assert ai._get_move(big_board) == 2
+
+def test_ai_can_stop_immediate_horizontal_loss(ai, big_board):
+    big_board.mark_space(4, "o")
+    big_board.mark_space(5, "o")
+    big_board.mark_space(6, "o")
+    assert ai._get_move(big_board) == 5
+
+def xtest_ai_1st_move_when_2nd_is_corner_space_if_middle_taken(ai, big_board):
+    big_board.mark_space(4, "o")
+    assert ai._get_move(big_board) in [0, 2, 6, 8]
+
+def xtest_ai_prevents_opponent_from_fork_win(ai, big_board):
+    big_board.mark_space(0, "o")
+    big_board.mark_space(4, "x")
+    big_board.mark_space(8, "o")
+    assert ai._get_move(big_board) in [1, 3, 5, 7]
+
+def xtest_ai_prevents_opponent_from_fork_win_side_middle(ai, big_board):
+    big_board.mark_space(0, "o")
+    big_board.mark_space(4, "x")
+    big_board.mark_space(7, "o")
+    assert ai._get_move(big_board) not in [2, 1]
+
+def test_ai_ends_game_when_possible_16_space_board(ai, big_board):
+    big_board.mark_space(0, "o")
+    big_board.mark_space(1, "o")
+    big_board.mark_space(2, "o")
+    big_board.mark_space(12, "x")
+    big_board.mark_space(13, "x")
+    big_board.mark_space(14, "x")
+    assert ai._get_move(big_board) == 6
+
+def xtest_ai_prevents_opponent_from_fork_win_middle_two_corners(ai, big_board):
+    big_board.mark_space(4, "o")
+    big_board.mark_space(8, "x")
+    big_board.mark_space(0, "o")
+    assert ai._get_move(big_board) in [2, 6]

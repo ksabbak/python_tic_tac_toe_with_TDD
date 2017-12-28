5a37bae84ea5ad57cd63e6b38edf67b7e30f96d6
K. Sabbak <ksabbak@gmail.com>
2017-12-28 14:31:34 -0600

    Removed pending 4x4 tests for forked wins since the larger board seems to prevent forked wins from being a thing.

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -117,22 +117,6 @@ def test_ai_can_stop_immediate_horizontal_loss_16_space_board(ai, big_board):
     big_board.mark_space(6, "o")
     assert ai._get_move(big_board) == 7
 
-def xtest_ai_1st_move_when_2nd_is_corner_space_if_middle_taken(ai, big_board):
-    big_board.mark_space(4, "o")
-    assert ai._get_move(big_board) in [0, 2, 6, 8]
-
-def xtest_ai_prevents_opponent_from_fork_win(ai, big_board):
-    big_board.mark_space(0, "o")
-    big_board.mark_space(4, "x")
-    big_board.mark_space(8, "o")
-    assert ai._get_move(big_board) in [1, 3, 5, 7]
-
-def xtest_ai_prevents_opponent_from_fork_win_side_middle(ai, big_board):
-    big_board.mark_space(0, "o")
-    big_board.mark_space(4, "x")
-    big_board.mark_space(7, "o")
-    assert ai._get_move(big_board) not in [2, 1]
-
 def test_ai_ends_game_when_possible_16_space_board(ai, big_board):
     big_board.mark_space(0, "o")
     big_board.mark_space(1, "o")
@@ -141,9 +125,3 @@ def test_ai_ends_game_when_possible_16_space_board(ai, big_board):
     big_board.mark_space(13, "x")
     big_board.mark_space(14, "x")
     assert ai._get_move(big_board) == 15
-
-def xtest_ai_prevents_opponent_from_fork_win_middle_two_corners(ai, big_board):
-    big_board.mark_space(4, "o")
-    big_board.mark_space(8, "x")
-    big_board.mark_space(0, "o")
-    assert ai._get_move(big_board) in [2, 6]

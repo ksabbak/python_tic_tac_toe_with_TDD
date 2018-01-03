11d68a50976b09aac00aa2ac27439a66160e5d63
K. Sabbak <ksabbak@gmail.com>
2018-01-03 14:13:05 -0600

    Build basic template structure

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from . import app
 
-@app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+@app.route('/')
+@app.route('/3x3')
+def three_x_three_board():
+    return render_template('board.html')

1a24d3a294b82a1ac6f5f9b542a27f96e4e59370
K. Sabbak <ksabbak@gmail.com>
2018-01-04 15:54:22 -0600

    Switch from query strings to form. Add game over functionality. Achieve M-ist of VPs.

--- web_app/routes.py
+++ web_app/routes.py
@@ -8,14 +8,31 @@ def index():
 
 @app.route('/')
 @app.route('/board')
-def three_x_three_board(board=None):
+def board(board=None):
     if board is not None:
         board = Board.create_from_existing(board)
     else: 
         board = Board.create_from_scratch()
     return render_template('board.html', board=board)
 
-@app.route('/board/<path:path>')
+@app.route('/board', methods=['POST'])
+def update_board():
+    board = request.form['board']
+    board = Board.create_from_existing(board)
+    board.mark_space(int(request.form['choice']), "x")
+    if board.is_full():
+        return ("Game over, no one wins.")
+    ai = AI("o")
+    ai.make_move(board)
+    if board.winning_marker() == "x":
+        return ('You won!')
+    elif board.winning_marker() == "o":
+        return ("You lost!")
+    else:
+        return render_template('board.html', board=board)
+
+
+@app.route('/board/<int:path>')
 def three_x_three_choice(path):
     print(path)
     board = request.args.get('board')

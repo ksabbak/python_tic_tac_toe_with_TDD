2687f8efb9bd27e166c8f6ef729547a228734363
K. Sabbak <ksabbak@gmail.com>
2018-01-18 13:36:12 -0600

    Attempt to write tests for Flask, fail.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+from setuptools import setup
+
 setup(
     name='TicTacToe',
     packages=['web_app', 'app'],
     include_package_data=True,
-    install_requires=['flask']
+    install_requires=['flask'],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
     )

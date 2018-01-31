363cb39e8d02739fb50208ac233f5c720827e934
K. Sabbak <ksabbak@gmail.com>
2018-01-31 14:46:05 -0600

    Add classmethod decorator to Colorist

--- tictactoe/app/command_line_views/colorist.py
+++ tictactoe/app/command_line_views/colorist.py
@@ -1,4 +1,5 @@
 class Colorist:
+    @classmethod
     def colors(cls):
         colors = {
                     "BLACK" : "\033[1;30m",
@@ -12,22 +13,26 @@ class Colorist:
                  }
         return colors
 
+    @classmethod
     def color_option_string(cls):
         color_string = "\n"
         for text, color in Colorist.colors().items():
             color_string += Colorist.color_text(text, color) + "\n"
         return color_string
 
+    @classmethod
     def color_text(cls, text, color):
         color = Colorist.assign_color_value(color)
         no_color = "\033[0m"
         return (color + text + no_color)
 
+    @classmethod
     def uncolor_text(cls, text, color):
         color = Colorist.assign_color_value(color)
         no_color = "\033[0m"
         return no_color + text + color
 
+    @classmethod
     def assign_color_value(cls, color):
         if color.upper() in Colorist.colors().keys():
             color = Colorist.colors()[color.upper()]

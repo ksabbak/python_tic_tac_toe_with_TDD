5ccde1861cb4a16f24c07bd33e06b09c05c1501d
ksabbak <ksabbak@gmail.com>
2018-02-13 11:29:59 -0600

    Tests for validator

--- tictactoe/tests/helpers.py
+++ tictactoe/tests/helpers.py
@@ -5,3 +5,8 @@ def mock_get_game_input(mock_input):
     def getter():
         return mock_input
     return getter
+
+def mock_for_validator(mock_input):
+    def getter():
+        return mock_input.pop()
+    return getter
--- tictactoe/tests/test_validator.py
+++ tictactoe/tests/test_validator.py
@@ -3,12 +3,50 @@ import unittest
 from unittest.mock import patch
 
 from tictactoe.app.validator import Validator
+from tictactoe.tests.helpers import mock_for_validator
 
+def test_validator_requires_valid_input_for_acceptable_game_type():
+    mock = mock_for_validator(['100', '1'])
+    valid_input = Validator().handle_input(mock, "game_type")
+    assert valid_input is not '100'
+    assert valid_input is '1'
 
-def xtest_controller_choses_proper_game():
-    with unittest.mock.patch('builtins.input', return_value='1'):
-        assert Controller().run() in "Human vs. Human!"
-    with unittest.mock.patch('builtins.input', return_value='2'):
-        assert Controller().run() in "Human vs. Computer!"
-    with unittest.mock.patch('builtins.input', return_value='3'):
-        assert Controller().run() in "Computer vs. Computer!"
+def test_validator_requires_valid_input_for_acceptable_board_type():
+    mock = mock_for_validator(['100', '1'])
+    valid_input = Validator().handle_input(mock, "board_type")
+    assert valid_input is not '100'
+    assert valid_input is '1'
+
+def test_validator_requires_valid_input_for_acceptable_color_name():
+    mock = mock_for_validator(['reed', 'red'])
+    valid_input = Validator().handle_input(mock, "color_name")
+    assert valid_input is not 'reed'
+    assert valid_input is 'red'
+
+def test_validator_is_case_insensitve_for_color_name():
+    mock = mock_for_validator(['GreEn'])
+    valid_input = Validator().handle_input(mock, "color_name")
+    assert valid_input is 'GreEn'
+
+def test_validator_accepts_none_color():
+    mock = mock_for_validator(['None'])
+    valid_input = Validator().handle_input(mock, "color_name")
+    assert valid_input is 'None'
+
+def test_validator_requires_valid_marker_input():
+    mock = mock_for_validator(['reed', ' ', '  ', 'x'])
+    valid_input = Validator().handle_input(mock, "marker_input")
+    assert valid_input is not 'reed'
+    assert valid_input is not ' '
+    assert valid_input is not '  '
+    assert valid_input is 'x'
+
+def test_validator_no_matches_returns_true_if_list_has_no_matches():
+    lst = [1, 2, 3, 4, 5]
+    no_matches = Validator().no_matches(lst)
+    assert no_matches == True
+
+def test_validator_no_matches_returns_none_if_list_has_matches():
+    lst = [1, 2, 3, 4, 1]
+    no_matches = Validator().no_matches(lst)
+    assert no_matches == None
